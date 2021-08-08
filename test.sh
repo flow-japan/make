26da770777f8099d5ac846b6b8d272df4d8a3329
avcdsld <avcdsld@gmail.com>
2021-08-09 02:47:45 +0900

    change contract spec

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 
 flow project deploy
 # flow project deploy -n testnet
+# flow accounts remove-contract Collectible -n testnet --signer testnetAccount1
 
-flow transactions send ./cadence/transactions/create_collectible_data.cdc \
+flow transactions send ./cadence/transactions/mint_nfts.cdc \
   --args-json '[
     {
       "type": "Dictionary",
@@ -21,29 +22,22 @@ flow transactions send ./cadence/transactions/create_collectible_data.cdc \
           "value": { "type": "String", "value": "https://ipfs.fleek.co/ipfs/bafybeid26vj2bvsatvcifk5p2mxsamhhc6lhw3jnw6cphmu34s5pxj2qhy" }
         }
       ]
-    },
-    {"type": "UInt64", "value": "100"}
+    }
   ]'
-
-flow scripts execute ./cadence/scripts/get_created_collectible_data.cdc --arg Address:f8d6e0586b0a20c7
-
-# Gas 制限のため、9999 に設定しても、一度に mint できるのは 300 程度まで
-flow transactions send ./cadence/transactions/mint_nfts.cdc \
-  --arg UInt64:1 --arg Int:100 \
-  --gas-limit 9999
 
 flow scripts execute ./cadence/scripts/get_all_metadata.cdc --arg Address:f8d6e0586b0a20c7
 
+flow transactions send ./cadence/transactions/list_nft.cdc \
+  --arg UInt64:1
+
+flow scripts execute ./cadence/scripts/get_showcase_all_metadata.cdc
 
 ###################
-
-# テストネットのコントラクト
-https://flow-view-source.com/testnet/account/0x85875109cfe22e4a/contract/Collectible
-
-# テストネットの tx 例
-
+## テストネットのコントラクト
+# https://flow-view-source.com/testnet/account/0x85875109cfe22e4a/contract/Collectible
+#
+## テストネットの tx 例
+#
 ## CollectibleData の作成
-https://flow-view-source.com/testnet/tx/aa06f2f81e1b9dc0dc9824a847ff31b965338504b33ef3e5c86dc44b994d56e1
-
-## NFT の mint
+# https://flow-view-source.com/testnet/tx/aa06f2f81e1b9dc0dc9824a847ff31b965338504b33ef3e5c86dc44b994d56e1
 

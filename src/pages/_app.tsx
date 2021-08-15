09277bda8623dfa1e48b00b6f2e78233ef047905
avcdsld <avcdsld@gmail.com>
2021-08-15 22:27:48 +0900

    change favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import Head from 'next/head';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 import theme from '../theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: false,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Make! NFT on Flow</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: false,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
